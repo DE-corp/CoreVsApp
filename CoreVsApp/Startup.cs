2ba6611e29a9df1a7d7fb239e2092c1601aeca0e
DE-corp <crazyHOPHOP@rambler.ru>
2023-08-11 18:45:13 +0300

    Создал кастомное middleware для логирования в консоль

--- CoreVsApp/Startup.cs
+++ CoreVsApp/Startup.cs
@@ -32,28 +32,23 @@ namespace CoreVsApp
 
             app.UseRouting();
 
-            //Используем метод Use, чтобы запрос передавался дальше по конвейеру
-            app.Use(async (context, next) =>
-            {
-                // Строка для публикации в лог
-                string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+            ////Используем метод Use, чтобы запрос передавался дальше по конвейеру
+            //app.Use(async (context, next) =>
+            //{
+            //    // Строка для публикации в лог
+            //    string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
 
-                // Путь до лога (опять-таки, используем свойства IWebHostEnvironment)
-                string logFilePath = Path.Combine(env.ContentRootPath, "Logs", "RequestLog.txt");
+            //    // Путь до лога (опять-таки, используем свойства IWebHostEnvironment)
+            //    string logFilePath = Path.Combine(env.ContentRootPath, "Logs", "RequestLog.txt");
 
-                // Используем асинхронную запись в файл
-                await File.AppendAllTextAsync(logFilePath, logMessage);
+            //    // Используем асинхронную запись в файл
+            //    await File.AppendAllTextAsync(logFilePath, logMessage);
 
-                await next.Invoke();
-            });
+            //    await next.Invoke();
+            //});
 
-            //Добавляем компонент для логирования запросов с использованием метода Use.
-            app.Use(async (context, next) =>
-            {
-                // Для логирования данных о запросе используем свойства объекта HttpContext
-                Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
-                await next.Invoke();
-            });
+            // Подключаем логирвоание с использованием ПО промежуточного слоя
+            app.UseMiddleware<LoggingMiddleware>();
 
             //Добавляем компонент с настройкой маршрутов для главной страницы
             app.UseEndpoints(endpoints =>
